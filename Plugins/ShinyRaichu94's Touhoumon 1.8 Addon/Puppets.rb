a8503c04dd4ad78407dbd582732e9af9da2ed069
ShinyRaichu94 <shinyraichu94@hotmail.com>
2026-06-17 13:11:31 -0400

    CYoumu, CYukari, and Hard Mode

--- Plugins/ShinyRaichu94's Touhoumon 1.8 Addon/Puppets.rb
+++ Plugins/ShinyRaichu94's Touhoumon 1.8 Addon/Puppets.rb
@@ -1,16 +1,40 @@
 #===============================================================================
+# Added Evolutions (By DerxwnaKapsyla)
+#===============================================================================
+
+GameData::Evolution.register({
+  :id            => :LevelForceForm1,
+  :parameter     => Integer,
+  :level_up_proc => proc { |pkmn, parameter|
+    next pkmn.level >= parameter
+  },
+  :after_evolution_proc => proc { |pkmn, new_species, parameter, evo_species|
+    next false if evo_species != new_species
+    pkmn.form = 1
+    next true
+  }
+})
+
+GameData::Evolution.register({
+  :id            => :ItemForceForm1,
+  :parameter     => :Item,
+  :use_item_proc => proc { |pkmn, parameter, item|
+    next item == parameter
+  },
+  :after_evolution_proc => proc { |pkmn, new_species, parameter, evo_species|
+    next false if evo_species != new_species 
+    pkmn.form = 1   # Force Form 1
+    next true
+  }
+})
+
+#===============================================================================
 # Form Handlers
 #===============================================================================
 
 #-------------------------------------------------------------------------------
-# Goliath (with Hourai's Level Up Moveset)
+# Goliath
 #-------------------------------------------------------------------------------
-
-MultipleForms.register(:HOURAI, {
-  "getForm" => proc { |pkmn|
-    next 1
-  }
-})
 
 MultipleForms.register(:GOLIATH, {
   "getFormOnCreation" => proc { |pkmn|
